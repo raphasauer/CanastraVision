47b05ef2c06b9e0c1f7becd40fff2a0338bd8f7b
Luana Campos <luanac-campos@hotmail.com>
2022-02-18 10:19:33 -0300

    bug fixed

--- script.py
+++ script.py
@@ -7,5 +7,10 @@ with open('dataset.csv', 'w') as file:
 	csv_writer = csv.writer(file)
 	for path in l:
 		label = path.split('_')
-		label = label[0][0] + label[2][0]
+		
+		if not label[0].isnumeric():
+			label = label[0][0] + label[2][0]
+		else:
+			label = label[0] + label[2][0]
+
 		csv_writer.writerow(['TRAINING', path, label, 0, 0, 1, 1])
